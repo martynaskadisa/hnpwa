12baf91aa3d86a3d4b2c7626db1b7e0d5deaf547
Martynas Kadiša <8273932+martynaskadisa@users.noreply.github.com>
2018-08-20 08:51:35 +0300

    add test

--- src/common/store/modules/device/sagas.test.ts
+++ src/common/store/modules/device/sagas.test.ts
@@ -1,5 +1,5 @@
-import configureStore from '../../configure-store';
-import { detectNetworkStatus } from './sagas';
+import configureStore from 'common/store/configure-store';
+import { detectNetworkStatus, watchNetworkStatus } from './sagas';
 
 const setOnline = (value: boolean) =>
   Object.defineProperty(window.navigator, 'onLine', {
@@ -14,7 +14,7 @@ describe('device sagas', () => {
         process.env.TARGET = 'browser';
       });
 
-      beforeEach(jest.resetModules);
+      afterAll(() => (process.env.TARGET = undefined));
 
       it('should set online to true when `window.navigator.onLine` is true', () => {
         setOnline(true);
@@ -36,6 +36,20 @@ describe('device sagas', () => {
         const state = store.getState();
 
         expect(state.device.online).toBe(false);
+      });
+    });
+
+    describe('node', () => {
+      beforeAll(() => (process.env.TARGET = 'node'));
+
+      it('should always return true', () => {
+        const store = configureStore({ device: { online: false } });
+
+        store.runSaga(detectNetworkStatus);
+
+        const state = store.getState();
+
+        expect(state.device.online).toBe(true);
       });
     });
   });
