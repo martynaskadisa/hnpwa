e2b9769c7a845c4a8ae4c335cebcd285798ca65e
Martynas Kadiša <8273932+martynaskadisa@users.noreply.github.com>
2018-08-27 23:44:35 +0300

    code style

--- src/common/hocs/withVisibility.tsx
+++ src/common/hocs/withVisibility.tsx
@@ -1,9 +1,13 @@
-import * as React from 'react';
 import { getComponentName } from 'common/utils/component';
+import * as React from 'react';
 
-export type VisibilityProps<T> = { isVisible: false } | T & { isVisible: boolean };
+export type VisibilityProps<T extends {}> =
+  | { isVisible: false }
+  | T & { isVisible: boolean };
 
-export const withVisibility = <T extends {}>(Component: React.ComponentType<T>) => {
+export const withVisibility = <T extends {}>(
+  Component: React.ComponentType<T>
+) => {
   const wrapper: React.SFC<VisibilityProps<T>> = props => {
     if (!props.isVisible) {
       return null;
